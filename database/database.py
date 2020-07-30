ba84cf6b01a3f8512efb9b626e55608af2dee79b
Revanth Mopidevi <39304828+revanthmopidevi@users.noreply.github.com>
2020-07-30 23:02:56 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,11 +1,12 @@
 import csv
-import passgen
+# replace with your choice of module
+import password_generator 
 from cs50 import SQL
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
-rdb = SQL("sqlite:///database.db")
-db = SQL("sqlite:///project.db")
+rdb = SQL("sqlite:///raw.db")
+db = SQL("sqlite:///database.db")
 
 with open('voter_list.csv', "r") as csvfile:
     reader = csv.reader(csvfile)
@@ -16,7 +17,7 @@ with open('voter_list.csv', "r") as csvfile:
                 if emailID[i] == '@':
                     break
             loginID = emailID[:i]
-            password = passgen.passgen(length=15, punctuation=True, digits=True, letters=True, case='both')
+            password = password_generator.generate()
             pw_hash = bcrypt.generate_password_hash(password)
             rdb.execute("INSERT INTO raw (loginID, emailID, password, hash) VALUES(:login, :email, :pwd, :hash)", login=loginID, email=emailID, pwd=password, hash = pw_hash)
             db.execute("INSERT INTO credentials (loginID, password) VALUES(:ID, :password)", ID=loginID, password=pw_hash)    
